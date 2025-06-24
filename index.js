c5f3a6bb09b20bc7826a75db7ec865020b8d893e
little-scale <seb.tomczak@gmail.com>
2025-06-24 10:25:12 +0930

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+console.log("Database URL:", process.env.DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
+
+
 
 // GET /concerts
 app.get("/concerts", async (req, res) => {
